36a54b939c5a794e64c1eb527b8269c6feb3f419
tyl3n <123915347+amcnabbbaltar@users.noreply.github.com>
2023-04-04 08:11:35 -0400

    Animated AI

--- Assets/Script/AIScript.cs
+++ Assets/Script/AIScript.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
+enum State {
+    Patrol,
+    LookingAround
+}
 public class AIScript : MonoBehaviour
 {
 
     public float maxAngle = 45;
     public float maxDistance = 10;
-    public float timer = 1.0f;
+    public float timer = 0.0f;
     public float visionCheckRate = 1.0f;
+    State state = State.Patrol;
     public Transform[] points;
     public Animator animator;
     private int destPoint = 0;
@@ -28,11 +33,20 @@ public class AIScript : MonoBehaviour
     void Update()
     { 
         UpdateAnimator();
+        timer += Time.deltaTime;
         if (!agent.pathPending && agent.remainingDistance < 0.5f)
         {
-            GotoNextPoint();
+            
+            if (state != State.LookingAround && !IsInvoking("GotoNextPoint") && timer­>3.0f)
+            {
+                state = State.LookingAround;
+                Debug.Log("Invoke");
+                agent.isStopped = true;
+                Invoke("GotoNextPoint", 5.0f);
+            }
         }
     }
+
     void UpdateAnimator()
     {
         if(agent.velocity.magnitude< 1) 
@@ -43,8 +57,12 @@ public class AIScript : MonoBehaviour
         {
             animator.SetFloat("CharacterSpeed",1.0f);
         }
+        animator.SetBool("IsLookingAround",state == State.LookingAround);
     }
     void GotoNextPoint() {
+        state = State.Patrol;
+        agent.isStopped = false;
+        timer = 0.0f;
         // Returns if no points have been set up
         if (points.Length == 0)
             return;
